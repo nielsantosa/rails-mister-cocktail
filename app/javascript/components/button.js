db4d519f2c49e076e7da6fb2b869a612073a549b
Nathaniel Santosa <nathaniel.santosa@gmail.com>
2021-02-22 18:07:14 +0800

    Update the css

---
+++ app/javascript/components/button.js
@@ -0,0 +1,8 @@
+const initUpdateNavbarOnScroll = () => {
+  console.log("Do this")
+  const reviewCard = document.querySelector('.review-card');
+  const deleteButton = document.querySelector('.review-card .delete-button')
+
+}
+
+export { initUpdateNavbarOnScroll };
